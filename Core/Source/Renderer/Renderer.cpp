2234cbaabf16368473ac3dab3ccb605b47e0c5f6
Anton ST <anton.stapen@gmail.com>
2026-04-19 15:04:54 +0200

    created vulkan instance

--- Core/Source/Renderer/Renderer.cpp
+++ Core/Source/Renderer/Renderer.cpp
@@ -1,9 +1,7 @@
 #include "Renderer.h"
 #include <iostream>
-
-#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
-#include <vulkan/vulkan_raii.hpp>
 #include <cassert>
+#include <ranges>
 
 using namespace Mupfel;
 
@@ -14,6 +12,7 @@ void Renderer::Init()
 {
 	std::cout << "Renderer::Init" << std::endl;
 	InitWindow();
+	InitVulkan();
 }
 
 void Renderer::Render()
@@ -47,4 +46,66 @@ void Mupfel::Renderer::InitWindow()
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	/* Create the window */
 	window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+}
+
+void Mupfel::Renderer::InitVulkan()
+{
+	createInstance();
+}
+
+void Mupfel::Renderer::createInstance()
+{
+	constexpr vk::ApplicationInfo appInfo{
+		.pApplicationName = "My first App",
+		.applicationVersion = VK_MAKE_VERSION(0, 1, 0),
+		.pEngineName = "No Engine",
+		.engineVersion = VK_MAKE_VERSION(1, 0, 0),
+		.apiVersion = vk::ApiVersion14 };
+
+	vk::InstanceCreateInfo createInfo{
+		.pApplicationInfo = &appInfo
+	};
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	auto extensionProperties = context.enumerateInstanceExtensionProperties();
+
+	/* Check if required GLFW extensions are supported by the vulkan implementation */
+	for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+	{
+		if (std::ranges::none_of(extensionProperties,
+			[glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+			{ return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+		{
+			throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+		}
+	}
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+	// Get the required layers
+	std::vector<char const*> requiredLayers;
+	if (enableValidationLayers)
+	{
+		requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+	}
+
+	// Check if the required layers are supported by the Vulkan implementation.
+	auto layerProperties = context.enumerateInstanceLayerProperties();
+	auto unsupportedLayerIt = std::ranges::find_if(requiredLayers,
+		[&layerProperties](auto const& requiredLayer) {
+			return std::ranges::none_of(layerProperties,
+				[requiredLayer](auto const& layerProperty) { return strcmp(layerProperty.layerName, requiredLayer) == 0; });
+		});
+	if (unsupportedLayerIt != requiredLayers.end())
+	{
+		throw std::runtime_error("Required layer not supported: " + std::string(*unsupportedLayerIt));
+	}
+	
+	createInfo.enabledLayerCount = static_cast<uint32_t>(requiredLayers.size());
+	createInfo.ppEnabledLayerNames = requiredLayers.data();
+
+	instance = vk::raii::Instance(context, createInfo);
 }
--- Core/Source/Renderer/Renderer.h
+++ Core/Source/Renderer/Renderer.h
@@ -1,6 +1,9 @@
 #pragma once
-#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan_raii.hpp>
 
 namespace Mupfel {
 
@@ -12,8 +15,23 @@ namespace Mupfel {
 		void DeInit();
 	private:
 		void InitWindow();
+		void InitVulkan();
+		void createInstance();
 	private:
 		GLFWwindow *window = nullptr;
+
+		vk::raii::Context context;
+		vk::raii::Instance instance = nullptr;
+
+		const std::vector<char const*> validationLayers = {
+			"VK_LAYER_KHRONOS_validation"
+		};
+
+#ifdef NDEBUG
+		const bool enableValidationLayers = false;
+#else
+		const bool enableValidationLayers = true;
+#endif
 	};
 
 }
