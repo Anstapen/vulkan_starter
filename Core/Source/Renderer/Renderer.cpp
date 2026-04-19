28a9d8ed7f8128be6e9d6ef4f97c8be5ca27515c
Anton ST <anton.stapen@gmail.com>
2026-04-19 18:40:23 +0200

    created swap chain and image views

--- Core/Source/Renderer/Renderer.cpp
+++ Core/Source/Renderer/Renderer.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <cassert>
 #include <ranges>
+#include <cstdint>
+#include <limits>
+#include <algorithm>
 
 using namespace Mupfel;
 
@@ -51,6 +54,10 @@ void Mupfel::Renderer::InitWindow()
 void Mupfel::Renderer::InitVulkan()
 {
 	createInstance();
+	createSurface();
+	pickPhysicalDevice();
+	createLogicalDevice();
+	createSwapChain();
 }
 
 void Mupfel::Renderer::createInstance()
@@ -108,4 +115,186 @@ void Mupfel::Renderer::createInstance()
 	createInfo.ppEnabledLayerNames = requiredLayers.data();
 
 	instance = vk::raii::Instance(context, createInfo);
+}
+
+std::vector<const char*> requiredDeviceExtension = { vk::KHRSwapchainExtensionName };
+
+bool Renderer::isDeviceSuitable(vk::raii::PhysicalDevice const& physicalDevice)
+{
+	// Check if the physicalDevice supports the Vulkan 1.3 API version
+	bool supportsVulkan1_3 = physicalDevice.getProperties().apiVersion >= vk::ApiVersion13;
+
+	// Check if any of the queue families support graphics operations
+	auto queueFamilies = physicalDevice.getQueueFamilyProperties();
+	bool supportsGraphics = std::ranges::any_of(queueFamilies, [](auto const& qfp) { return !!(qfp.queueFlags & vk::QueueFlagBits::eGraphics); });
+
+	// Check if all required physicalDevice extensions are available
+	auto availableDeviceExtensions = physicalDevice.enumerateDeviceExtensionProperties();
+	bool supportsAllRequiredExtensions =
+		std::ranges::all_of(requiredDeviceExtension,
+			[&availableDeviceExtensions](auto const& requiredDeviceExtension)
+			{
+				return std::ranges::any_of(availableDeviceExtensions,
+					[requiredDeviceExtension](auto const& availableDeviceExtension)
+					{ return strcmp(availableDeviceExtension.extensionName, requiredDeviceExtension) == 0; });
+			});
+
+	// Check if the physicalDevice supports the required features (dynamic rendering and extended dynamic state)
+	auto features =
+		physicalDevice
+		.template getFeatures2<vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features, vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
+	bool supportsRequiredFeatures = features.template get<vk::PhysicalDeviceVulkan13Features>().dynamicRendering &&
+		features.template get<vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>().extendedDynamicState;
+
+	// Return true if the physicalDevice meets all the criteria
+	return supportsVulkan1_3 && supportsGraphics && supportsAllRequiredExtensions && supportsRequiredFeatures;
+}
+
+void Renderer::pickPhysicalDevice()
+{
+	std::vector<vk::raii::PhysicalDevice> physicalDevices = instance.enumeratePhysicalDevices();
+	auto const devIter = std::ranges::find_if(physicalDevices, [&](auto const& physicalDevice) { return isDeviceSuitable(physicalDevice); });
+	if (devIter == physicalDevices.end())
+	{
+		throw std::runtime_error("failed to find a suitable GPU!");
+	}
+	physicalDevice = *devIter;
+	std::cout << "Device Name: " << physicalDevice.getProperties().deviceName.data() << std::endl;
+}
+
+void Mupfel::Renderer::createLogicalDevice() {
+	// find the index of the first queue family that supports graphics
+	std::vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+
+	// get the first index into queueFamilyProperties which supports both graphics and present
+	uint32_t queueIndex = ~0;
+	for (uint32_t qfpIndex = 0; qfpIndex < queueFamilyProperties.size(); qfpIndex++)
+	{
+		if ((queueFamilyProperties[qfpIndex].queueFlags & vk::QueueFlagBits::eGraphics) &&
+			physicalDevice.getSurfaceSupportKHR(qfpIndex, *surface))
+		{
+			// found a queue family that supports both graphics and present
+			queueIndex = qfpIndex;
+			break;
+		}
+	}
+	if (queueIndex == ~0)
+	{
+		throw std::runtime_error("Could not find a queue for graphics and present -> terminating");
+	}
+
+	// query for Vulkan 1.3 features
+	vk::StructureChain<vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features, vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT> featureChain = {
+		{},                                   // vk::PhysicalDeviceFeatures2
+		{.dynamicRendering = true},           // vk::PhysicalDeviceVulkan13Features
+		{.extendedDynamicState = true}        // vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT
+	};
+
+	// create a Device
+	float                     queuePriority = 0.5f;
+	vk::DeviceQueueCreateInfo deviceQueueCreateInfo{ .queueFamilyIndex = queueIndex, .queueCount = 1, .pQueuePriorities = &queuePriority };
+	vk::DeviceCreateInfo      deviceCreateInfo{ .pNext = &featureChain.get<vk::PhysicalDeviceFeatures2>(),
+											   .queueCreateInfoCount = 1,
+											   .pQueueCreateInfos = &deviceQueueCreateInfo,
+											   .enabledExtensionCount = static_cast<uint32_t>(requiredDeviceExtension.size()),
+											   .ppEnabledExtensionNames = requiredDeviceExtension.data() };
+
+	device = vk::raii::Device(physicalDevice, deviceCreateInfo);
+	queue = vk::raii::Queue(device, queueIndex, 0);
+}
+
+void Renderer::createSurface() {
+	VkSurfaceKHR       _surface;
+	if (glfwCreateWindowSurface(*instance, window, nullptr, &_surface) != 0) {
+		throw std::runtime_error("failed to create window surface!");
+	}
+	surface = vk::raii::SurfaceKHR(instance, _surface);
+}
+
+vk::SurfaceFormatKHR Mupfel::Renderer::chooseSwapSurfaceFormat(const std::vector<vk::SurfaceFormatKHR>& availableFormats) {
+	const auto formatIt = std::ranges::find_if(
+		availableFormats,
+		[](const auto& format) { return format.format == vk::Format::eB8G8R8A8Srgb && format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear; });
+	return formatIt != availableFormats.end() ? *formatIt : availableFormats[0];
+}
+
+vk::PresentModeKHR Mupfel::Renderer::chooseSwapPresentMode(std::vector<vk::PresentModeKHR> const& availablePresentModes)
+{
+	assert(std::ranges::any_of(availablePresentModes, [](auto presentMode) { return presentMode == vk::PresentModeKHR::eFifo; }));
+	return std::ranges::any_of(availablePresentModes,
+		[](const vk::PresentModeKHR value) { return vk::PresentModeKHR::eMailbox == value; }) ?
+		vk::PresentModeKHR::eMailbox :
+		vk::PresentModeKHR::eFifo;
+}
+
+vk::Extent2D Mupfel::Renderer::chooseSwapExtent(vk::SurfaceCapabilitiesKHR const& capabilities)
+{
+	if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+	{
+		return capabilities.currentExtent;
+	}
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	return {
+		std::clamp<uint32_t>(width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
+		std::clamp<uint32_t>(height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height)
+	};
+}
+
+void Mupfel::Renderer::createSwapChain()
+{
+	vk::SurfaceCapabilitiesKHR surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(surface);
+	std::vector<vk::SurfaceFormatKHR> availableFormats = physicalDevice.getSurfaceFormatsKHR(surface);
+	std::vector<vk::PresentModeKHR> avaiablePresentModes = physicalDevice.getSurfacePresentModesKHR(surface);
+
+	swapChainExtent = chooseSwapExtent(surfaceCapabilities);
+	swapChainSurfaceFormat = chooseSwapSurfaceFormat(availableFormats);
+	vk::PresentModeKHR chosenPresentMode = chooseSwapPresentMode(avaiablePresentModes);
+	uint32_t imageCount = chooseSwapMinImageCount(surfaceCapabilities);
+
+	vk::SwapchainCreateInfoKHR swapChainCreateInfo{
+		.surface = surface,
+		.minImageCount = imageCount,
+		.imageFormat = swapChainSurfaceFormat.format,
+		.imageColorSpace = swapChainSurfaceFormat.colorSpace,
+		.imageArrayLayers = 1,
+		.imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+		.imageSharingMode = vk::SharingMode::eExclusive,
+		.preTransform = surfaceCapabilities.currentTransform,
+		.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+		.presentMode = chosenPresentMode,
+		.clipped = true };
+	swapChainCreateInfo.oldSwapchain = nullptr;
+	swapChainCreateInfo.imageExtent = swapChainExtent;
+
+	swapChain = vk::raii::SwapchainKHR(device, swapChainCreateInfo);
+	swapChainImages = swapChain.getImages();
+}
+
+void Mupfel::Renderer::createImageViews()
+{
+	assert(swapChainImageViews.empty());
+
+	vk::ImageViewCreateInfo imageViewCreateInfo = {
+		.viewType = vk::ImageViewType::e2D,
+		.format = swapChainSurfaceFormat.format,
+		.subresourceRange = {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1}
+	};
+
+	for (auto& image : swapChainImages)
+	{
+		imageViewCreateInfo.image = image;
+		swapChainImageViews.emplace_back(device, imageViewCreateInfo);
+	}
+}
+
+uint32_t Mupfel::Renderer::chooseSwapMinImageCount(vk::SurfaceCapabilitiesKHR const& surfaceCapabilities)
+{
+	auto minImageCount = std::max(3u, surfaceCapabilities.minImageCount);
+	if ((0 < surfaceCapabilities.maxImageCount) && (surfaceCapabilities.maxImageCount < minImageCount))
+	{
+		minImageCount = surfaceCapabilities.maxImageCount;
+	}
+	return minImageCount;
 }
--- Core/Source/Renderer/Renderer.h
+++ Core/Source/Renderer/Renderer.h
@@ -1,6 +1,7 @@
 #pragma once
+
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
 
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #include <vulkan/vulkan_raii.hpp>
@@ -17,11 +18,32 @@ namespace Mupfel {
 		void InitWindow();
 		void InitVulkan();
 		void createInstance();
+		void pickPhysicalDevice();
+		bool isDeviceSuitable(vk::raii::PhysicalDevice const& physicalDevice);
+		void createLogicalDevice();
+		void createSurface();
+		vk::SurfaceFormatKHR chooseSwapSurfaceFormat(std::vector<vk::SurfaceFormatKHR> const& availableFormats);
+		vk::PresentModeKHR chooseSwapPresentMode(std::vector<vk::PresentModeKHR> const& availablePresentModes);
+		vk::Extent2D chooseSwapExtent(vk::SurfaceCapabilitiesKHR const& capabilities);
+		uint32_t chooseSwapMinImageCount(vk::SurfaceCapabilitiesKHR const& surfaceCapabilities);
+		void createSwapChain();
+		void createImageViews();
+
 	private:
 		GLFWwindow *window = nullptr;
 
 		vk::raii::Context context;
 		vk::raii::Instance instance = nullptr;
+		vk::raii::PhysicalDevice physicalDevice = nullptr;
+		vk::raii::Device device = nullptr;
+		vk::raii::Queue queue = nullptr;
+		vk::raii::SurfaceKHR surface = nullptr;
+
+		vk::raii::SwapchainKHR swapChain = nullptr;
+		std::vector<vk::Image> swapChainImages;
+		vk::SurfaceFormatKHR   swapChainSurfaceFormat;
+		vk::Extent2D           swapChainExtent;
+		std::vector<vk::raii::ImageView> swapChainImageViews;
 
 		const std::vector<char const*> validationLayers = {
 			"VK_LAYER_KHRONOS_validation"
